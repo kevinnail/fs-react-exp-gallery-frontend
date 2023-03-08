ac1d14665eecc1d61d3d86a7280cbbceed14ead9
kevinnail <fumalicious@gmail.com>
2023-03-08 14:22:06 -0800

    edited user check in app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Auth from './components/Auth/Auth.js';
+import Admin from './components/Admin/Admin.js';
+import Header from './components/Header/Header.js';
 import { useUser } from './hooks/useUser.js';
 
 function App() {
-  const { user, error } = useUser();
+  const { user } = useUser();
 
-  if (user) {
-    return <Redirect to="/admin" />;
-  } else if (error) {
-    console.error(error);
-  }
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route path="/auth/:type" component={Auth} />
+        <Route exact path="/admin" component={Admin} />
         <Route exact path="*" />
         {user && <Redirect to="/admin" />}
         {!user && <Redirect to="/auth/sign-in" />}
