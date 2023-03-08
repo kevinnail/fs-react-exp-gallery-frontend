7cad9d322cb957f77dd52d3f8d74564f0d488c8a
kevinnail <fumalicious@gmail.com>
2023-03-08 10:48:49 -0800

    Fixed redirect, added check for user

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Auth from './components/Auth/Auth.js';
+import { useUser } from './hooks/useUser.js';
 
 function App() {
+  const { user, error } = useUser();
+
+  if (user) {
+    return <Redirect to="/gallery" />;
+  } else if (error) {
+    console.error(error);
+  }
   return (
     <div className="App">
       <Switch>
         <Route path="/auth/:type" component={Auth} />
-        <Route exact path="/" />
-        {user && <Redirect to="/posts" />}
+        <Route exact path="*" />
+        {user && <Redirect to="/gallery" />}
         {!user && <Redirect to="/auth/sign-in" />}
       </Switch>
     </div>
   );
 }
-
 export default App;
