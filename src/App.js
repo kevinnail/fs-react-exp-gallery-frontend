227755d1d9f299ec691e9aa3691ea34de323d686
kevinnail <fumalicious@gmail.com>
2023-03-08 21:31:32 -0800

    added new post route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Auth from './components/Auth/Auth.js';
 import Admin from './components/Admin/Admin.js';
 import Header from './components/Header/Header.js';
 import { useUser } from './hooks/useUser.js';
+import NewPost from './components/NewPost/NewPost.js';
 
 function App() {
   const { user } = useUser();
@@ -13,6 +14,7 @@ function App() {
       <Header />
       <Switch>
         <Route exact path="/auth/:type" component={Auth} />
+        <Route path="/admin/new" component={NewPost} />
         <Route path="/admin" component={Admin} />
         <Route exact path="*">
           {user && <Redirect to="/admin" />}
