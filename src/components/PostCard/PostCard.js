36893c4af129a5d146f28ffcdcb3065049ce9d84
kevinnail <fumalicious@gmail.com>
2023-03-09 15:36:55 -0800

    added each value to (basic) display

--- src/components/PostCard/PostCard.js
+++ src/components/PostCard/PostCard.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import { Link, Redirect } from 'react-router-dom';
 // import { usePost } from '../../hooks/usePost.js';
 import { useUser } from '../../hooks/useUser.js';
@@ -6,7 +6,16 @@ import { useUser } from '../../hooks/useUser.js';
 // import './PostCard.css';
 
 // export default function PostCard({ task, id, completed, setPosts, posts }) {
-export default function PostCard({ id, title, description, image_url, price, category, user_id }) {
+export default function PostCard({
+  id,
+  posts,
+  title,
+  description,
+  image_url,
+  category,
+  price,
+  author_id,
+}) {
   const { user } = useUser();
   // const { setLoading, setError } = usePost(id);
   // const [isCompleted, setIsCompleted] = useState(completed);
@@ -15,61 +24,44 @@ export default function PostCard({ id, title, description, image_url, price, cat
     return <Redirect to="/auth/sign-in" />;
   }
 
+  //   return (
+  //     <div className="post overlay" key={id}>
+  //       {title}
+  //     </div>
+  //   );
+  // }
+
+  // delete the post
+  // const handleDelete = async () => {
+  //   try {
+  //     await deleteById(id);
+  //     const updatedPosts = posts.filter((post) => post.id !== id);
+  //     setPosts(updatedPosts);
+  //     setLoading(true);
+  //   } catch (e) {
+  //     setError(e.message);
+  //   }
+  // };
+
+  // make the post card clickable and toggle the completed status
+  const handleEdit = async () => {
+    console.log('handleEdit');
+  };
+
   return (
     <div className="post overlay" key={id}>
-      {title}
-      {description}
-      {image_url}
-      {price}
-      {category}
-      {user_id}
+      <Link className="buttons btn-align" to={`/admin/edit/${id}`}>
+        <img src="/edit.png" className="edit-button" alt="edit" />{' '}
+      </Link>
+      {/* <Link className="buttons red-border" to={`/admin/${id}`} onClick={handleDelete}>
+        <img className=" " onClick={() => {}} src="/delete.png" name="delete" alt="delete" />
+      </Link> */}
+      <h1>title: {title}</h1>
+      <p>desc: {description}</p>
+      <p>img: {image_url}</p>
+      <p>cat: {category}</p>
+      <p>$ {price}</p>
+      <p>ID: {id}</p>
     </div>
   );
 }
-
-//
-//
-//
-// delete the post
-// const handleDelete = async () => {
-//   try {
-//     await deleteById(id);
-//     const updatedPosts = posts.filter((post) => post.id !== id);
-//     setPosts(updatedPosts);
-//     setLoading(true);
-//   } catch (e) {
-//     setError(e.message);
-//   }
-// };
-
-// make the post card clickable and toggle the completed status
-// const handleEdit = async () => {
-//   await toggleComplete(!isCompleted, id);
-//   setIsCompleted(!isCompleted);
-// };
-
-//   return (
-//     <div className="post overlay" key={id}>
-//       <Link className="buttons btn-align" to={`/admin/edit/${id}`}>
-//         <img src="/edit.png" className="edit-button" alt="edit" />{' '}
-//       </Link>
-//       <Link className="buttons red-border" to={`/admin/${id}`} onClick={handleDelete}>
-//         <img className=" " onClick={() => {}} src="/delete.png" name="delete" alt="delete" />
-//       </Link>
-//       <h1 onClick={() => handleEdit()} className={isCompleted ? 'completed-todo' : 'todo'} id={id}>
-//         {isCompleted ? (
-//           <>
-//             <img className="unchecked-box" src="./unchecked.png" />
-//             <img className="checked" src="./finished.png" />
-//             <span className="task-span">{task}</span>
-//           </>
-//         ) : (
-//           <>
-//             <img className="unchecked" src="./unchecked.png" />
-//             <span className="task-span">{task}</span>
-//           </>
-//         )}
-//       </h1>
-//     </div>
-//   );
-// }
