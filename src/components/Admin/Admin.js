5cdc1051c5bb0bb731c9a6694845e89eb64097e6
kevinnail <fumalicious@gmail.com>
2023-03-10 16:44:30 -0800

    added css and Admin label

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -3,6 +3,7 @@ import { Redirect } from 'react-router-dom';
 import { usePosts } from '../../hooks/usePosts.js';
 import { useUser } from '../../hooks/useUser.js';
 import PostCard from '../PostCard/PostCard.js';
+import './Admin.css';
 
 export default function Admin() {
   const { user } = useUser();
@@ -20,10 +21,18 @@ export default function Admin() {
     );
   }
   return (
-    <div className="list-container">
-      {posts.map((post) => (
-        <PostCard key={post.id} {...post} setPosts={setPosts} posts={posts} />
-      ))}
-    </div>
+    <>
+      <div className="admin">ADMIN</div>
+      <div className="prod-header">
+        <span className="grid-2">Title</span>
+        <span className="grid-5">Price</span>
+      </div>
+
+      <div className="list-container">
+        {posts.map((post) => (
+          <PostCard key={post.id} {...post} setPosts={setPosts} posts={posts} />
+        ))}
+      </div>
+    </>
   );
 }
