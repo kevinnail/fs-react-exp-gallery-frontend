e8d6bacfdf8bd44c0a02ce2b61452ec4ec35eb64
kevinnail <fumalicious@gmail.com>
2023-03-18 02:01:49 -0700

    cleaning up postcard display...

--- src/components/GalleryPostCard/GalleryPostCard.js
+++ src/components/GalleryPostCard/GalleryPostCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './GalleryPostCard.css';
-import { usePost } from '../../hooks/usePost.js';
-import { useUser } from '../../hooks/useUser.js';
+// import { usePost } from '../../hooks/usePost.js';
+// import { useUser } from '../../hooks/useUser.js';
 
 export default function GalleryPostCard({
   id,
@@ -14,7 +14,7 @@ export default function GalleryPostCard({
 }) {
   return (
     <div className="gallery-display" key={id}>
-      <p className="gallery-title">{title.length > 9 ? title.slice(0, 9) + '...' : title}</p>
+      <p className="gallery-title">{title.length > 20 ? title.slice(0, 20) + '...' : title}</p>
       <img className="gallery-img" src={image_url} alt="gallery image" />
       <p className="gallery-price">${price}</p>
     </div>
