bd8cdb34404000d8a3c86a7d2cb728df19f72691
kevinnail <fumalicious@gmail.com>
2023-03-17 22:55:53 -0700

    removed admin div and labels from form

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -22,7 +22,7 @@ export default function Admin() {
   }
   return (
     <>
-      <div className="admin">ADMIN</div>
+      {/* <div className="admin">ADMIN</div> */}
       <div className="prod-header">
         <span className="grid-2">Title</span>
         <span className="grid-5">Price</span>
--- src/components/PostForm/PostForm.js
+++ src/components/PostForm/PostForm.js
@@ -47,104 +47,107 @@ export default function PostForm({
   };
 
   return (
-    <form className="new-post-form" onSubmit={handleFormSubmit} encType="multipart/form-data">
-      <h1 id="form-title-header">{newOrEdit}</h1>
-      <div>
-        <label className="form-title" htmlFor="category">
-          Category
-        </label>
-        <br />
-        <select
-          id="category"
-          value={categoryInput}
-          onChange={handleCategoryChange}
-          className="image-input shadow-border"
-          required
-        >
-          <option value="" disabled selected>
-            Choose category
-          </option>
-          <option value="Beads">Beads</option>
-          <option value="Blunt Tips">Blunt Tips</option>
-          <option value="Bubblers">Bubblers</option>
-          <option value="Collabs">Collabs</option>
-          <option value="Cups">Cups</option>
-          <option value="Goblets">Goblets</option>
-          <option value="Iso Stations">Iso Stations</option>
-          <option value="Marbles">Marbles</option>
-          <option value="Dry Pieces">Dry Pieces</option>
-          <option value="Pendants">Pendants</option>
-          <option value="Recyclers">Recyclers</option>
-          <option value="Rigs">Rigs</option>
-          <option value="Slides">Slides</option>
-          <option value="Spinner Caps">Spinner Caps</option>
-          <option value="Terp Pearls">Terp Pearls</option>
-          <option value="Misc">Misc</option>
-        </select>
-      </div>
-      <div>
-        <label className="form-title">Title</label>
-        <br />
-        <input
-          required
-          maxLength={50}
-          placeholder="Enter title"
-          className="image-input"
-          type="text"
-          name="title"
-          value={titleInput}
-          onChange={(e) => setTitleInput(e.target.value)}
-        />
-      </div>
-      <div>
-        <label className="form-title">Description</label>
-        <br />
-        <textarea
-          required
-          maxLength={350}
-          placeholder="Enter description"
-          className="image-input description shadow-border"
-          name="description"
-          value={descriptionInput}
-          onChange={(e) => setDescriptionInput(e.target.value)}
-        />
-      </div>
+    <>
+      {/* <div className="admin">ADMIN</div> */}
+      <form className="new-post-form" onSubmit={handleFormSubmit} encType="multipart/form-data">
+        <h1 id="form-title-header">{newOrEdit}</h1>
+        <div>
+          {/* <label className="form-title" htmlFor="category">
+            Category
+          </label> */}
+          <br />
+          <select
+            id="category"
+            value={categoryInput}
+            onChange={handleCategoryChange}
+            className="image-input shadow-border"
+            required
+          >
+            <option value="" disabled selected>
+              Choose category
+            </option>
+            <option value="Beads">Beads</option>
+            <option value="Blunt Tips">Blunt Tips</option>
+            <option value="Bubblers">Bubblers</option>
+            <option value="Collabs">Collabs</option>
+            <option value="Cups">Cups</option>
+            <option value="Goblets">Goblets</option>
+            <option value="Iso Stations">Iso Stations</option>
+            <option value="Marbles">Marbles</option>
+            <option value="Dry Pieces">Dry Pieces</option>
+            <option value="Pendants">Pendants</option>
+            <option value="Recyclers">Recyclers</option>
+            <option value="Rigs">Rigs</option>
+            <option value="Slides">Slides</option>
+            <option value="Spinner Caps">Spinner Caps</option>
+            <option value="Terp Pearls">Terp Pearls</option>
+            <option value="Misc">Misc</option>
+          </select>
+        </div>
+        <div>
+          {/* <label className="form-title">Title</label> */}
+          <br />
+          <input
+            required
+            maxLength={50}
+            placeholder="Enter title"
+            className="image-input"
+            type="text"
+            name="title"
+            value={titleInput}
+            onChange={(e) => setTitleInput(e.target.value)}
+          />
+        </div>
+        <div>
+          {/* <label className="form-title">Description</label> */}
+          <br />
+          <textarea
+            required
+            maxLength={350}
+            placeholder="Enter description"
+            className="image-input description shadow-border"
+            name="description"
+            value={descriptionInput}
+            onChange={(e) => setDescriptionInput(e.target.value)}
+          />
+        </div>
 
-      <div>
-        <label className="form-title">Price</label>
-        <br />
-        <input
-          required
-          placeholder="Enter price"
-          className="image-input"
-          type="number"
-          step="1"
-          name="price"
-          value={priceInput}
-          onChange={(e) => setPriceInput(e.target.value)}
-        />
-      </div>
+        <div>
+          {/* <label className="form-title">Price</label> */}
+          <br />
+          <input
+            required
+            placeholder="Enter price"
+            className="image-input"
+            type="number"
+            step="1"
+            name="price"
+            value={priceInput}
+            onChange={(e) => setPriceInput(e.target.value)}
+          />
+        </div>
 
-      <div>
-        <label className="form-title " htmlFor="image">
-          Images
-        </label>
-        <br />
-        <input
-          type="file"
-          id="image"
-          className="file-upload-btn shadow-border"
-          name="image"
-          onChange={handleFileInputChange}
-          multiple
-        />
-      </div>
+        <div>
+          {/* <label className="form-title " htmlFor="image">
+            Images
+          </label> */}
+          <br />
+          <input
+            type="file"
+            id="image"
+            className="file-upload-btn shadow-border"
+            name="image"
+            onChange={handleFileInputChange}
+            multiple
+          />
+        </div>
 
-      <div className="btn-container">
-        <button className="submit-btn" type="submit">
-          Submit
-        </button>
-      </div>
-    </form>
+        <div className="btn-container">
+          <button className="submit-btn" type="submit">
+            {<img className="upload-icon " src="/upload.png" alt="upload" />}
+          </button>
+        </div>
+      </form>
+    </>
   );
 }
