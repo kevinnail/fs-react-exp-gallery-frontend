0b13454d35ce0afeaff43a6e9178a31dfdbde1b4
kevinnail <fumalicious@gmail.com>
2023-03-18 01:25:51 -0700

    add basic gallery layout

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { usePosts } from '../../hooks/usePosts.js';
+import { useUser } from '../../hooks/useUser.js';
 import GalleryPostCard from '../GalleryPostCard/GalleryPostCard.js';
+import './Gallery.css';
 
 export default function Gallery() {
+  const { user } = useUser();
+  const { posts, loading } = usePosts();
+
+  if (loading) {
+    return (
+      <div className="loading">
+        <h1>Loading! One moment please!</h1>
+      </div>
+    );
+  }
   return (
     <>
-      {'hello from Gallery'}
-      <GalleryPostCard />
+      <div className="gallery-list-container">
+        {posts.map((post) => (
+          <GalleryPostCard key={post.id} {...post} posts={posts} />
+        ))}
+      </div>
     </>
   );
 }
