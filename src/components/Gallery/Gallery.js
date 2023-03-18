03621bb528a633fa6c319c59d62af4e697baf82c
kevinnail <fumalicious@gmail.com>
2023-03-18 00:40:59 -0700

    add Gallery component

---
+++ src/components/Gallery/Gallery.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import GalleryPostCard from '../GalleryPostCard/GalleryPostCard.js';
+
+export default function Gallery() {
+  return (
+    <>
+      {'hello from Gallery'}
+      <GalleryPostCard />
+    </>
+  );
+}
