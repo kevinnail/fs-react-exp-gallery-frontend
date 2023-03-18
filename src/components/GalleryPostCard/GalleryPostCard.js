4710ef429be13bb7a89aa291f2be58a37810b859
kevinnail <fumalicious@gmail.com>
2023-03-18 01:47:01 -0700

    add css cleaned up gallery posts for basic display

--- src/components/GalleryPostCard/GalleryPostCard.js
+++ src/components/GalleryPostCard/GalleryPostCard.js
@@ -5,27 +5,18 @@ import { useUser } from '../../hooks/useUser.js';
 
 export default function GalleryPostCard({
   id,
-  posts,
   title,
-  description, //commented just for now
-  image_url, //commented just for now
-  category, //commented just for now
+  image_url,
   price,
+  // posts,
+  // description, //commented just for now
+  // category, //commented just for now
 }) {
-  // const { user } = useUser();
-  // const { setError } = usePost(id);
   return (
     <div className="gallery-display" key={id}>
-      <img className="gallery-img" src={image_url} alt="edit" />
-      <p className="grid-s2 grid-e4 ">{title.length > 9 ? title.slice(0, 9) + '...' : title}</p>
-      <p className="grid-5">${price}</p>
-      <div className="admin-prod-btn-cont grid-7"></div>
-      {/* <h1>title: {title}</h1>
-  <p>desc: {description}</p>
-  <p>img: {image_url}</p>
-  <p>cat: {category}</p>
-  <p>$ {price}</p>
-  <p>ID: {id}</p> */}
+      <p className="gallery-title">{title.length > 9 ? title.slice(0, 9) + '...' : title}</p>
+      <img className="gallery-img" src={image_url} alt="gallery image" />
+      <p className="gallery-price">${price}</p>
     </div>
   );
 }
