fe2f7182a1a1ec441b14b2e8ed621f19c4255d6c
kevinnail <fumalicious@gmail.com>
2023-03-19 02:15:27 -0700

    incremental display adjustment in postDetail

--- src/components/PostDetail/PostDetail.js
+++ src/components/PostDetail/PostDetail.js
@@ -9,15 +9,17 @@ export default function PostDetail() {
   const { postDetail, imageUrls, loading, error } = usePost(id);
   // const { postDetail } = post;
 
+  if (loading) return <h1>Loading...</h1>;
+
   return (
     <div className="post-detail-div">
       <h1>{postDetail.title}</h1>
+      <p>{postDetail.category}</p>
+      <p>{postDetail.description}</p>
+      <p>${postDetail.price}</p>
       {imageUrls.map((imageUrl, index) => (
         <img className="gallery-image" key={index} src={imageUrl} alt={`post-${index}`} />
       ))}
-      <p>{postDetail.description}</p>
-      <p>${postDetail.price}</p>
-      <p>{postDetail.category}</p>
     </div>
   );
 }
