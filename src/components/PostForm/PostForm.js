8fbae97f8e6873788a920d09f19cb1f54f3477a1
kevinnail <fumalicious@gmail.com>
2023-03-19 11:38:35 -0700

    add loading state/ animated  logo img to PostForm

--- src/components/PostForm/PostForm.js
+++ src/components/PostForm/PostForm.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useUser } from '../../hooks/useUser.js';
 import { uploadImagesAndCreatePost } from '../../services/fetch-utils.js';
+// import { usePosts } from '../../hooks/usePosts.js';
 import './PostForm.css';
 
 export default function PostForm({
@@ -18,6 +19,7 @@ export default function PostForm({
   const { user } = useUser();
   const newOrEdit = title ? 'Edit Post' : 'New Gallery Post';
   const [loading, setLoading] = useState(false);
+  // const [loading, setLoading] = usePosts();
 
   const handleFileInputChange = (event) => {
     setImageFilesInput([...event.target.files]);
@@ -42,7 +44,7 @@ export default function PostForm({
     } catch (error) {
       console.error(error);
     } finally {
-      setLoading(false);
+      // setLoading(false);
     }
   };
 
@@ -50,102 +52,111 @@ export default function PostForm({
     setCategoryInput(event.target.value);
   };
 
+  if (loading) {
+    return (
+      <div className="loading-div">
+        <img className="loading" src="../logo-sq.png" />
+      </div>
+    );
+  }
   return (
     <>
-      {loading ? (
-        <div>Loading...</div> // You can use any loading indicator here
-      ) : (
-        <form className="new-post-form" onSubmit={handleFormSubmit} encType="multipart/form-data">
-          <h1 id="form-title-header">{newOrEdit}</h1>
-          <div>
-            <br />
-            <select
-              id="category"
-              value={categoryInput}
-              onChange={handleCategoryChange}
-              className="image-input shadow-border"
-              required
-            >
-              <option value="" disabled>
-                Choose category
-              </option>
-              <option value="Beads">Beads</option>
-              <option value="Blunt Tips">Blunt Tips</option>
-              <option value="Bubblers">Bubblers</option>
-              <option value="Collabs">Collabs</option>
-              <option value="Cups">Cups</option>
-              <option value="Dry Pieces">Dry Pieces</option>
-              <option value="Goblets">Goblets</option>
-              <option value="Iso Stations">Iso Stations</option>
-              <option value="Marbles">Marbles</option>
-              <option value="Pendants">Pendants</option>
-              <option value="Recyclers">Recyclers</option>
-              <option value="Rigs">Rigs</option>
-              <option value="Slides">Slides</option>
-              <option value="Spinner Caps">Spinner Caps</option>
-              <option value="Terp Pearls">Terp Pearls</option>
-              <option value="Misc">Misc</option>
-            </select>
-          </div>
-          <div>
-            <br />
-            <input
-              required
-              maxLength={50}
-              placeholder="Enter title"
-              className="image-input"
-              type="text"
-              name="title"
-              value={titleInput}
-              onChange={(e) => setTitleInput(e.target.value)}
-            />
-          </div>
-          <div>
-            <br />
-            <textarea
-              required
-              maxLength={350}
-              placeholder="Enter description"
-              className="image-input description shadow-border"
-              name="description"
-              value={descriptionInput}
-              onChange={(e) => setDescriptionInput(e.target.value)}
-            />
-          </div>
+      {/* {loading ? ( */}
+      {/* <div>
+          <img className="loading" src="../logo-sq.png" />
+        </div> // You can use any loading indicator here */}
+      {/* ) : ( */}
+      <form className="new-post-form" onSubmit={handleFormSubmit} encType="multipart/form-data">
+        <h1 id="form-title-header">{newOrEdit}</h1>
+        <div>
+          <br />
+          <select
+            id="category"
+            value={categoryInput}
+            onChange={handleCategoryChange}
+            className="image-input shadow-border"
+            required
+          >
+            <option value="" disabled>
+              Choose category
+            </option>
+            <option value="Beads">Beads</option>
+            <option value="Blunt Tips">Blunt Tips</option>
+            <option value="Bubblers">Bubblers</option>
+            <option value="Collabs">Collabs</option>
+            <option value="Cups">Cups</option>
+            <option value="Dry Pieces">Dry Pieces</option>
+            <option value="Goblets">Goblets</option>
+            <option value="Iso Stations">Iso Stations</option>
+            <option value="Marbles">Marbles</option>
+            <option value="Pendants">Pendants</option>
+            <option value="Recyclers">Recyclers</option>
+            <option value="Rigs">Rigs</option>
+            <option value="Slides">Slides</option>
+            <option value="Spinner Caps">Spinner Caps</option>
+            <option value="Terp Pearls">Terp Pearls</option>
+            <option value="Misc">Misc</option>
+          </select>
+        </div>
+        <div>
+          <br />
+          <input
+            required
+            maxLength={50}
+            placeholder="Enter title"
+            className="image-input"
+            type="text"
+            name="title"
+            value={titleInput}
+            onChange={(e) => setTitleInput(e.target.value)}
+          />
+        </div>
+        <div>
+          <br />
+          <textarea
+            required
+            maxLength={350}
+            placeholder="Enter description"
+            className="image-input description shadow-border"
+            name="description"
+            value={descriptionInput}
+            onChange={(e) => setDescriptionInput(e.target.value)}
+          />
+        </div>
 
-          <div>
-            <br />
-            <input
-              required
-              placeholder="Enter price"
-              className="image-input"
-              type="number"
-              step="1"
-              name="price"
-              value={priceInput}
-              onChange={(e) => setPriceInput(e.target.value)}
-            />
-          </div>
+        <div>
+          <br />
+          <input
+            required
+            placeholder="Enter price"
+            className="image-input"
+            type="number"
+            step="1"
+            name="price"
+            value={priceInput}
+            onChange={(e) => setPriceInput(e.target.value)}
+          />
+        </div>
 
-          <div>
-            <br />
-            <input
-              type="file"
-              id="image"
-              className="file-upload-btn shadow-border"
-              name="image"
-              onChange={handleFileInputChange}
-              multiple
-            />
-          </div>
+        <div>
+          <br />
+          <input
+            type="file"
+            id="image"
+            className="file-upload-btn shadow-border"
+            name="image"
+            onChange={handleFileInputChange}
+            multiple
+          />
+        </div>
 
-          <div className="btn-container">
-            <button className="submit-btn" type="submit">
-              {<img className="upload-icon " src="/upload.png" alt="upload" />}
-            </button>
-          </div>
-        </form>
-      )}
+        <div className="btn-container">
+          <button className="submit-btn" type="submit">
+            {<img className="upload-icon " src="/upload.png" alt="upload" />}
+          </button>
+        </div>
+      </form>
+      {/* )} */}
     </>
   );
 }
