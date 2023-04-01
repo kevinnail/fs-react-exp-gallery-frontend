bcb2eac6cf13800e5bbe83d0d8eb602bc716a78d
kevinnail <fumalicious@gmail.com>
2023-04-01 15:16:44 -0700

    add modal to post detail display

--- src/components/PostDetail/PostDetail.js
+++ src/components/PostDetail/PostDetail.js
@@ -1,23 +1,27 @@
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
+import Modal from 'react-modal';
 import { usePost } from '../../hooks/usePost.js';
 import './PostDetail.css';
+
+Modal.setAppElement('#root'); // If your app is using #root as the main container
 
 export default function PostDetail() {
   const { id } = useParams();
   const { postDetail, imageUrls, loading, error } = usePost(id);
   const [currentIndex, setCurrentIndex] = useState(0);
-
-  const handleSwipe = (direction) => {
-    if (direction === 'left' && currentIndex < imageUrls.length - 1) {
-      setCurrentIndex((prevIndex) => prevIndex + 1);
-    } else if (direction === 'right' && currentIndex > 0) {
-      setCurrentIndex((prevIndex) => prevIndex - 1);
-    }
-  };
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
   const handleThumbnailClick = (index) => {
     setCurrentIndex(index);
+  };
+
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
   };
 
   if (error) return <h1>{error}</h1>;
@@ -41,13 +45,27 @@ export default function PostDetail() {
         <span className="price-details">${postDetail.price}</span>
       </section>
       <div className="gallery-container">
-        <button className="swipe-button" onClick={() => handleSwipe('right')}>
-          &lt;
-        </button>
-        <img className="gallery-image" src={imageUrls[currentIndex]} alt={`post-${currentIndex}`} />
-        <button className="swipe-button" onClick={() => handleSwipe('left')}>
-          &gt;
-        </button>
+        <img
+          className="gallery-image"
+          src={imageUrls[currentIndex]}
+          alt={`post-${currentIndex}`}
+          onClick={openModal}
+        />
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          className="modal"
+          overlayClassName="overlay2"
+        >
+          <img
+            className="modal-image"
+            src={imageUrls[currentIndex]}
+            alt={`modal-post-${currentIndex}`}
+          />
+          <button className="modal-close" onClick={closeModal}>
+            x
+          </button>
+        </Modal>
       </div>
       <div className="thumbnail-container">
         {imageUrls.map((imageUrl, index) => (
