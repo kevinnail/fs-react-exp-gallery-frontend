f46ebb2b99d6de9200bee8482260f71df40f213e
kevinnail <fumalicious@gmail.com>
2023-04-01 14:33:56 -0700

    added image box for image display

--- src/components/PostDetail/PostDetail.js
+++ src/components/PostDetail/PostDetail.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { usePost } from '../../hooks/usePost.js';
 import './PostDetail.css';
@@ -6,8 +6,18 @@ import './PostDetail.css';
 export default function PostDetail() {
   const { id } = useParams();
   const { postDetail, imageUrls, loading, error } = usePost(id);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleSwipe = (direction) => {
+    if (direction === 'left' && currentIndex < imageUrls.length - 1) {
+      setCurrentIndex((prevIndex) => prevIndex + 1);
+    } else if (direction === 'right' && currentIndex > 0) {
+      setCurrentIndex((prevIndex) => prevIndex - 1);
+    }
+  };
+
   if (error) return <h1>{error}</h1>;
-  // show loading spinner while waiting for posts to load1
+  // show loading spinner while waiting for posts to load
   if (loading) {
     return (
       <div className="loading-div">
@@ -26,9 +36,15 @@ export default function PostDetail() {
         <span className="desc-details">{postDetail.description}</span>
         <span className="price-details">${postDetail.price}</span>
       </section>
-      {imageUrls.map((imageUrl, index) => (
-        <img className="gallery-image" key={index} src={imageUrl} alt={`post-${index}`} />
-      ))}
+      <div className="gallery-container">
+        <button className="swipe-button" onClick={() => handleSwipe('right')}>
+          &lt;
+        </button>
+        <img className="gallery-image" src={imageUrls[currentIndex]} alt={`post-${currentIndex}`} />
+        <button className="swipe-button" onClick={() => handleSwipe('left')}>
+          &gt;
+        </button>
+      </div>
     </div>
   );
 }
