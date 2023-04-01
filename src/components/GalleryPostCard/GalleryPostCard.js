8bc1b02d88fc6ad5e8350a4bffeb365a1e2ec407
kevinnail <fumalicious@gmail.com>
2023-04-01 02:05:53 -0700

    styled main gallery page

--- src/components/GalleryPostCard/GalleryPostCard.js
+++ src/components/GalleryPostCard/GalleryPostCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { usePost } from '../../hooks/usePost.js';
 import './GalleryPostCard.css';
 
 export default function GalleryPostCard({
@@ -10,15 +11,29 @@ export default function GalleryPostCard({
   // description, //commented just for now
   // category, //commented just for now
 }) {
+  const post = usePost(id);
+
   return (
     <>
-      <a href={`/gallery/${id}`}>
-        <div className="gallery-display" key={id}>
-          <p className="gallery-title">{title.length > 20 ? title.slice(0, 20) + '...' : title}</p>
-          <img className="gallery-img" src={image_url} alt="gallery image" />
-          <p className="gallery-price">${price}</p>
-        </div>
-      </a>
+      <div className="gallery-display-container" key={id}>
+        <a className="gallery-display" href={`/gallery/${id}`}>
+          <div className="gallery-detail-container">
+            <span className="gallery-title">
+              {title.length > 17 ? title.slice(0, 17) + '...' : title}
+            </span>
+            <span className="gallery-price">${price}</span>
+          </div>
+          {image_url ? (
+            <img className="gallery-img" src={image_url} alt="edit" />
+          ) : (
+            <>
+              {post.additionalImages[0] && (
+                <img className="gallery-img" src={post.additionalImages[0].image_url} alt="edit" />
+              )}
+            </>
+          )}
+        </a>
+      </div>
     </>
   );
 }
