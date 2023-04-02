b25ba4aa1e865a7fe46da0ae063900ca93223c52
kevinnail <fumalicious@gmail.com>
2023-04-01 22:58:58 -0700

    added animated cube of imgs on auth.js

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -7,9 +7,9 @@ export default function Auth() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [isSignIn, setIsSignIn] = useState(true);
-  const { user, logInUser, error } = useUser();
+  const { user, logInUser, error, loading, setLoading } = useUser();
   const { type } = useParams();
-  const [loading, setLoading] = useState(false);
+  // const [loading, setLoading] = useState(false);
   if (user) {
     return <Redirect to="/admin" />;
   } else if (error) {
@@ -29,60 +29,75 @@ export default function Auth() {
     return (
       <div className="loading-div">
         <img className="loading" src="../logo-sq.png" />
+        {/* <img className="rotating-marble" src="../marble-css.png" /> */}
       </div>
     );
   }
 
   return (
-    <div className="auth-container">
-      {}
-      {}
-      {}
-      {}
-      {}
-      {}
-      <div className="marble-container">
+    <>
+      <div className="auth-container">
+        {}
+        {}
+        {}
+        <div className="scene">
+          <div className="cube">
+            <div className="face front"></div>
+            <div className="face back"></div>
+            <div className="face right"></div>
+            <div className="face left"></div>
+            <div className="face top"></div>
+            <div className="face bottom"></div>
+          </div>
+        </div>
+        {}
+        {}
+        {}
+        {/* <div className="marble-container">
         <img className="rotating-marble" src="../marble-css.png" />
-      </div>
+      </div> */}
 
-      {}
-      {}
-      {}
-      {}
-      {}
-      {}
-      {}
-      <div className="sign-in-sign-out">
-        <NavLink className="auth-link" to="/auth/sign-in" onClick={() => setIsSignIn(true)}>
-          Sign-in
-        </NavLink>
-        <NavLink className="auth-link" to="/auth/sign-up" onClick={() => setIsSignIn(false)}>
-          Sign-up
-        </NavLink>
-      </div>
+        {}
+        {}
+        {}
+        {}
+        {}
+        {}
+        {}
+        <div className="auth-section-container">
+          <div className="sign-in-sign-out">
+            <NavLink className="auth-link" to="/auth/sign-in" onClick={() => setIsSignIn(true)}>
+              Sign-in
+            </NavLink>
+            <NavLink className="auth-link" to="/auth/sign-up" onClick={() => setIsSignIn(false)}>
+              Sign-up
+            </NavLink>
+          </div>
 
-      <div className="email-container">
-        <input
-          className="input-auth"
-          type="email"
-          placeholder="email@email.com"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
+          <div className="email-container">
+            <input
+              className="input-auth"
+              type="email"
+              placeholder="email@email.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
 
-        <input
-          className="input-auth"
-          type="password"
-          placeholder="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+          <input
+            className="input-auth"
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <div>
+            <button className="button-auth" onClick={submitAuth}>
+              {isSignIn ? 'Sign In' : 'Sign Up'}
+            </button>
+          </div>
+        </div>
       </div>
-      <div>
-        <button className="button-auth" onClick={submitAuth}>
-          {isSignIn ? 'Sign In' : 'Sign Up'}
-        </button>
-      </div>
-    </div>
+    </>
   );
 }
