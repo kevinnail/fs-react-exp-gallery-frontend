b4855657102aa6bd375e35dfe6ac277d21bd08aa
kevinnail <fumalicious@gmail.com>
2023-04-05 20:12:29 -0700

    admin page css mostly done, need adjustments

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import { Redirect } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { usePosts } from '../../hooks/usePosts.js';
 import { useUser } from '../../hooks/useUser.js';
 import PostCard from '../PostCard/PostCard.js';
 import './Admin.css';
+import { signOut } from '../../services/auth.js';
 
 export default function Admin() {
-  const { user } = useUser();
+  const { user, setUser } = useUser();
   const { posts, loading, setPosts } = usePosts();
 
   if (!user) {
     return <Redirect to="/auth/sign-in" />;
   }
+  const handleClick = async () => {
+    await signOut();
+    setUser(null);
+  };
 
   return (
     <>
@@ -20,7 +25,23 @@ export default function Admin() {
           <span className="grid-3">Title</span>
           <span className="grid-2">All items:</span>
         </div> */}
-        <div className="admin-panel">hi from inside the admin-panel</div>
+        <aside className="admin-panel ">
+          <section className="admin-panel-section ">
+            <div className="">
+              <Link className="new-link" to="/gallery">
+                <span className="new-post-span">Gallery</span>{' '}
+                {<img className="new-post-icon" src="../gallery.png" />}
+              </Link>
+              <Link className="new-link" to="/admin/new">
+                <span className="new-post-span">New Post</span>{' '}
+                {<img className="new-post-icon" src="../upload-1.png" />}
+              </Link>
+              <button className="signout-button" onClick={handleClick}>
+                Sign Out {<img className="signout-nav-icon" src="../signout.png" />}
+              </button>
+            </div>
+          </section>
+        </aside>
         <div className="list-container">
           {loading ? (
             <div className="loading-div">
