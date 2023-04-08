de9e0318ca23846dcd38679cc9bd9640f2021e3f
kevinnail <fumalicious@gmail.com>
2023-04-08 00:26:31 -0700

    make menu conditional about user

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useUser } from '../../hooks/useUser.js';
 
 export default function Menu({ handleClick }) {
+  const { user } = useUser();
   return (
     <>
       <Link className="new-link" to="/main-gallery">
@@ -12,13 +14,17 @@ export default function Menu({ handleClick }) {
         <span className="new-post-span menu-btn">About</span>{' '}
         {<img className="new-post-icon" src="../info.png" />}
       </Link>
-      <Link className="new-link" to="/admin/new">
-        <span className="new-post-span">New Post</span>{' '}
-        {<img className="new-post-icon" src="../upload-1.png" />}
-      </Link>
-      <button className="signout-button" onClick={handleClick}>
-        Sign Out {<img className="signout-nav-icon" src="../signout.png" />}
-      </button>
+      {user && (
+        <>
+          <Link className="new-link" to="/admin/new">
+            <span className="new-post-span">New Post</span>{' '}
+            {<img className="new-post-icon" src="../upload-1.png" />}
+          </Link>
+          <button className="signout-button" onClick={handleClick}>
+            Sign Out {<img className="signout-nav-icon" src="../signout.png" />}
+          </button>
+        </>
+      )}
     </>
   );
 }
