31bb42a3f14d28bc0d4e3fb75bd657ebc6666d05
kevinnail <fumalicious@gmail.com>
2023-04-08 10:44:24 -0700

    add signout function to mainGallery

--- src/components/MainGallery/MainGallery.js
+++ src/components/MainGallery/MainGallery.js
@@ -4,10 +4,14 @@ import { useGalleryPosts } from '../../hooks/useGalleryPosts.js';
 import MainGalleryPostCard from '../MainGalleryPostCard/MainGalleryPostCard.js';
 import '../Gallery/Gallery.css';
 import Menu from '../Menu/Menu.js';
+import { signOut } from '../../services/auth.js';
 // import './MainGallery.css';
+import { useUser } from '../../hooks/useUser.js';
 
 export default function MainGallery() {
   const { posts, loading } = useGalleryPosts();
+  const { setUser } = useUser();
+
   if (loading) {
     return (
       <div className="loading-div-wrapper">
@@ -18,10 +22,15 @@ export default function MainGallery() {
     );
   }
 
+  const handleClick = async () => {
+    await signOut();
+    setUser(null);
+  };
+
   return (
     <>
       <div className="menu-search-container">
-        <Menu />
+        <Menu handleClick={handleClick} />
       </div>
 
       <div className="gallery-list-container">
