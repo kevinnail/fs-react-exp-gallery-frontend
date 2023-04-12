cec3be470838616d25af3da981d0e1190d7783ef
kevinnail <fumalicious@gmail.com>
2023-04-11 23:14:23 -0700

    restore div wrapper for loading

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -3,10 +3,10 @@ import './Loading.css';
 
 export default function Loading() {
   return (
-    // <div className="loading-div-wrapper">
-    <div className="loading-div">
-      <img className="rotating-marble" src="../marble-css.png" />
+    <div className="loading-div-wrapper">
+      <div className="loading-div">
+        <img className="rotating-marble" src="../marble-css.png" />
+      </div>
     </div>
-    // </div>
   );
 }
