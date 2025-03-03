8b1d7cf87d84464520f8f446d6a582873440bdc0
kevinnail <fumalicious@gmail.com>
2025-03-02 22:52:56 -0800

    refactor to use mui

--- src/components/MainGallery/MainGallery.js
+++ src/components/MainGallery/MainGallery.js
@@ -7,6 +7,7 @@ import { signOut } from '../../services/auth.js';
 import { useUser } from '../../hooks/useUser.js';
 import Loading from '../Loading/Loading.js';
 import { getSiteMessage } from '../../services/fetch-utils.js';
+import { Box } from '@mui/material';
 
 //
 //
@@ -39,29 +40,29 @@ export default function MainGallery() {
 
   return (
     <>
-      <div className="menu-search-container">
+      <Box className="menu-search-container">
         <Menu handleClick={handleClick} />
-      </div>
-      <div className="up-top-msg">
+      </Box>
+      <Box className="up-top-msg" sx={{ width: '100%' }}>
         <span className="site-msg-span">{message}</span>
-        <div className="contact-wrapper">
+        <Box className="contact-wrapper">
           {' '}
           <span className="site-message-contact-header">Contact</span>
-          <div className="message-link-wrapper">
+          <Box className="message-link-wrapper">
             <a href={'mailto:kevin@kevinnail.com'}>
               <img className="site-msg-link-ig" width={'48px'} src="/email.png" />
             </a>
             <a href="https://www.instagram.com/stresslessglass">
               <img width={'48px'} src="/IG.png" />
             </a>
-          </div>
-        </div>
-      </div>
-      <div className="gallery-list-container">
+          </Box>
+        </Box>
+      </Box>
+      <Box className="gallery-list-container">
         {posts.map((post) => (
           <MainGalleryPostCard key={post.id} {...post} />
         ))}
-      </div>
+      </Box>
     </>
   );
 }
