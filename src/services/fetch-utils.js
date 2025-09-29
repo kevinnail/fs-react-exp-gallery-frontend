d20c648374850b5586cbb906bcb014ed5e2609ba
kevinnail <fumalicious@gmail.com>
2025-09-29 09:21:15 -0700

    replace hardcoded url with variable

--- src/services/fetch-utils.js
+++ src/services/fetch-utils.js
@@ -1,6 +1,6 @@
 /// urls for both local and deployed
 // const BASE_URL = 'http://localhost:7890';
-const BASE_URL = 'https://stresslessglass.kevinnail.com';
+const BASE_URL = process.env.REACT_APP_HOME_URL;
 
 /* Auth related functions */
 export async function getUser() {
