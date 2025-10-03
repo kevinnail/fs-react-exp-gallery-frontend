725dc11c20cb53041c07d96018335315e75f66aa
kevinnail <fumalicious@gmail.com>
2025-10-02 23:02:38 -0700

    add error handling with toast message

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -1,13 +1,27 @@
 import { useContext } from 'react';
 import { UserContext } from '../context/UserContext.js';
 import { authUser } from '../services/auth.js';
+import { toast } from 'react-toastify';
 
 export function useUser() {
   const { user, setUser, loading, error, setError, setLoading } = useContext(UserContext);
 
   const logInUser = async (email, password, type) => {
-    const user = await authUser(email, password, type);
-    setUser(user);
+    try {
+      const user = await authUser(email, password, type);
+      setUser(user);
+    } catch (e) {
+      toast.error(`Error logging in`, {
+        position: 'top-center',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'dark',
+      });
+    }
   };
 
   return { user, setUser, error, setError, logInUser, loading, setLoading };
