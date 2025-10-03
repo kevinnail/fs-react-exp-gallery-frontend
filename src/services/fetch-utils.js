ead940c369ae7413957c925771ba38d66d23be9f
kevinnail <fumalicious@gmail.com>
2025-10-02 22:28:17 -0700

    refactor fetch-utils, improve error handling

--- src/services/fetch-utils.js
+++ src/services/fetch-utils.js
@@ -1,80 +1,99 @@
 /// urls for both local and deployed
-// const BASE_URL = 'http://localhost:7890';
 const BASE_URL = process.env.REACT_APP_HOME_URL;
 
 /* Auth related functions */
 export async function getUser() {
-  const resp = await fetch(`${BASE_URL}/api/v1/users/me`, {
-    method: 'GET',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    credentials: 'include',
-  });
+  try {
+    const resp = await fetch(`${BASE_URL}/api/v1/users/me`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+    });
 
-  if (resp.ok) {
-    const user = await resp.json();
-    return user;
-  } else if (resp.status === 401 || resp.status === 403) {
-    return null;
-  } else {
-    throw new Error('An error occurred while fetching user');
+    if (resp.ok) {
+      const user = await resp.json();
+      return user;
+    } else if (resp.status === 401 || resp.status === 403) {
+      return null;
+    } else {
+      throw new Error('An error occurred while fetching user');
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
   }
 }
 
 export async function signUpUser(email, password) {
-  const resp = await fetch(`${BASE_URL}/api/v1/users`, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ email, password }),
-    credentials: 'include',
-  });
-  const data = await resp.json();
+  try {
+    const resp = await fetch(`${BASE_URL}/api/v1/users`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+      credentials: 'include',
+    });
+    const data = await resp.json();
 
-  if (resp.ok) {
-    await signInUser(email, password);
-    return resp;
-  } else {
-    // eslint-disable-next-line no-console
-    console.error(data.message);
+    if (resp.ok) {
+      await signInUser(email, password);
+      return resp;
+    } else {
+      // eslint-disable-next-line no-console
+      console.error(data.message);
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
   }
 }
 
 export async function signInUser(email, password) {
-  const resp = await fetch(`${BASE_URL}/api/v1/users/sessions`, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ email, password }),
-    credentials: 'include',
-  });
+  try {
+    const resp = await fetch(`${BASE_URL}/api/v1/users/sessions`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+      credentials: 'include',
+    });
 
-  const data = await resp.json();
+    const data = await resp.json();
 
-  if (resp.ok) {
-    location.replace('/admin ');
+    if (resp.ok) {
+      location.replace('/admin ');
 
-    return resp;
-  } else {
-    // eslint-disable-next-line no-console
-    console.error(data.message);
+      return resp;
+    } else {
+      // eslint-disable-next-line no-console
+      console.error(data.message);
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
   }
 }
 
 export async function signOutUser() {
-  const resp = await fetch(`${BASE_URL}/api/v1/users/sessions`, {
-    method: 'DELETE',
-    credentials: 'include',
-  });
+  try {
+    const resp = await fetch(`${BASE_URL}/api/v1/users/sessions`, {
+      method: 'DELETE',
+      credentials: 'include',
+    });
 
-  if (resp.ok) {
-    location.replace('/auth');
+    if (resp.ok) {
+      location.replace('/auth');
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
   }
 }
 
@@ -82,22 +101,27 @@ export async function signOutUser() {
 
 // get all posts from database and display on admin page
 export async function fetchPosts() {
-  const resp = await fetch(`${BASE_URL}/api/v1/admin`, {
-    method: 'GET',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    credentials: 'include',
-  });
-  const data = await resp.json();
+  try {
+    const resp = await fetch(`${BASE_URL}/api/v1/admin`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+    });
+    const data = await resp.json();
 
-  if (resp.ok) {
-    return data;
-  } else {
-    // eslint-disable-next-line no-console
-    // console.error(data.message);
-    throw new Error(data.message);
+    if (resp.ok) {
+      return data;
+    } else {
+      // eslint-disable-next-line no-console
+      // console.error(data.message);
+      throw new Error(data.message);
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
   }
 }
 
@@ -115,100 +139,128 @@ export async function postPost(
   hide,
   sold
 ) {
-  const resp = await fetch(`${BASE_URL}/api/v1/admin`, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      title,
-      description,
-      image_url,
-      category,
-      price,
-      author_id,
-      public_id,
-      num_imgs,
-      link,
-      hide,
-      sold,
-    }),
-    credentials: 'include',
-  });
+  try {
+    const resp = await fetch(`${BASE_URL}/api/v1/admin`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        title,
+        description,
+        image_url,
+        category,
+        price,
+        author_id,
+        public_id,
+        num_imgs,
+        link,
+        hide,
+        sold,
+      }),
+      credentials: 'include',
+    });
 
-  const msg = await resp.json();
-  if (resp.ok) {
-    return msg;
-  } else {
-    // eslint-disable-next-line no-console
-    console.error(msg.message);
-    throw new Error(msg.message);
+    const msg = await resp.json();
+    if (resp.ok) {
+      return msg;
+    } else {
+      // eslint-disable-next-line no-console
+      console.error(msg.message);
+      throw new Error(msg.message);
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
   }
 }
 
 // upload images to S3 and send urls and public ids to db
 export async function postAddImages(imageFiles, id) {
-  const formData = new FormData();
-  formData.append('image_urls', JSON.stringify(imageFiles.map((image) => image.secure_url)));
-  formData.append('image_public_ids', JSON.stringify(imageFiles.map((image) => image.public_id)));
-  formData.append('resource_types', JSON.stringify(imageFiles.map((image) => image.resource_type)));
-  // Append the id to the formData
-  formData.append('id', id);
-  const resp = await fetch(`${BASE_URL}/api/v1/admin/images`, {
-    method: 'POST',
-    body: formData,
-    credentials: 'include',
-  });
+  try {
+    const formData = new FormData();
+    formData.append('image_urls', JSON.stringify(imageFiles.map((image) => image.secure_url)));
+    formData.append('image_public_ids', JSON.stringify(imageFiles.map((image) => image.public_id)));
+    formData.append(
+      'resource_types',
+      JSON.stringify(imageFiles.map((image) => image.resource_type))
+    );
+    // Append the id to the formData
+    formData.append('id', id);
+    const resp = await fetch(`${BASE_URL}/api/v1/admin/images`, {
+      method: 'POST',
+      body: formData,
+      credentials: 'include',
+    });
 
-  const msg = await resp.json();
-  return msg;
+    const msg = await resp.json();
+    return msg;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 }
 
 // delete single post from database
 export async function deleteById(post_id) {
-  const resp = await fetch(`${BASE_URL}/api/v1/admin/${post_id}`, {
-    method: 'DELETE',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    credentials: 'include',
-  });
-  const msg = await resp.json();
+  try {
+    const resp = await fetch(`${BASE_URL}/api/v1/admin/${post_id}`, {
+      method: 'DELETE',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+    });
+    const msg = await resp.json();
 
-  return msg;
+    return msg;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 }
 
 // edit post called from EditPost
 export async function updatePost(id, post) {
-  const resp = await fetch(`${BASE_URL}/api/v1/admin/${id}`, {
-    method: 'PUT',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ id, post }),
-    credentials: 'include',
-  });
-  const msg = await resp.json();
+  try {
+    const resp = await fetch(`${BASE_URL}/api/v1/admin/${id}`, {
+      method: 'PUT',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ id, post }),
+      credentials: 'include',
+    });
+    const msg = await resp.json();
 
-  return msg;
+    return msg;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 }
 
 // return post detail (no image urls aside from the first one)
 export async function getPostDetail(id) {
-  const resp = await fetch(`${BASE_URL}/api/v1/admin/${id}`, {
-    method: 'GET',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    credentials: 'include',
-  });
+  try {
+    const resp = await fetch(`${BASE_URL}/api/v1/admin/${id}`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+    });
 
-  const msg = await resp.json();
-  return msg;
+    const msg = await resp.json();
+    return msg;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 }
 
 // image file upload functions
@@ -364,17 +416,23 @@ export const fetchGalleryPosts = async () => {
 
 // public route for post detail
 export async function getGalleryPostDetail(id) {
-  const resp = await fetch(`${BASE_URL}/api/v1/main-gallery/${id}`, {
-    method: 'GET',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-  });
+  try {
+    const resp = await fetch(`${BASE_URL}/api/v1/main-gallery/${id}`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    });
 
-  const msg = await resp.json();
-  return msg;
+    const msg = await resp.json();
+    return msg;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 }
+
 export const getAdditionalImageUrlsPublicIdsGallery = async (id) => {
   try {
     const response = await fetch(`${BASE_URL}/api/v1/main-gallery/urls/${id}`, {
