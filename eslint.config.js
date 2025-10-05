4998d89ac445d252296736ec3f739aa8d9c276eb
kevinnail <fumalicious@gmail.com>
2025-10-05 16:21:42 -0700

    update config file

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,30 @@ export default [
   js.configs.recommended,
   {
     files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      globals: {
+        window: 'readonly',
+        document: 'readonly',
+        console: 'readonly',
+        fetch: 'readonly',
+        FormData: 'readonly',
+        history: 'readonly',
+        test: 'readonly',
+        expect: 'readonly',
+        describe: 'readonly',
+        it: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly',
+        jest: 'readonly',
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
     plugins: {
       react,
       'react-hooks': reactHooks,
@@ -13,6 +37,7 @@ export default [
     rules: {
       ...react.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
+      'no-unused-vars': 'warn',
     },
     settings: {
       react: {
