3506b6f120aa43a303bb9f651abcca65494123c6
kevinnail <fumalicious@gmail.com>
2025-10-06 09:55:24 -0700

    remove pattern detection (handled in validation)

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -260,7 +260,6 @@ export default function Auth() {
                     required
                     inputMode="email"
                     autoComplete="email"
-                    pattern="^[^\s@]+@[^\s@]+\.[^\s@]+$"
                   />
                 </div>
 
