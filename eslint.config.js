ec76fce895cf36e0180de4f3bafe1059075862d9
kevinnail <fumalicious@gmail.com>
2025-10-05 20:23:36 -0700

    update eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
-const js = require('@eslint/js');
-const react = require('eslint-plugin-react');
-const reactHooks = require('eslint-plugin-react-hooks');
+import js from '@eslint/js';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
 
-module.exports = [
+export default [
   js.configs.recommended,
   {
     files: ['**/*.{js,jsx}'],
