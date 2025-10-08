f930e9c05185fb341359b935c7112ec8fcadcacf
kevinnail <fumalicious@gmail.com>
2025-10-08 02:51:03 -0700

    update home link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -32,11 +32,7 @@ export default function Header() {
   return (
     <>
       <header>
-        <Link
-          className="link"
-          to={user && isAdmin ? '/admin' : '/main-gallery'}
-          onClick={handleHomeClick}
-        >
+        <Link className="link" to={user && isAdmin ? '/admin' : '/'} onClick={handleHomeClick}>
           <img className="logo" src="../logo-sq.png" />
         </Link>
         <h1 className="title">Stress Less Glass</h1>
