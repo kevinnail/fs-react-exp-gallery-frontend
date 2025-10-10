b85aa1ca2bd4b8b1d75c6762baf309bae8a7fd85
kevinnail <fumalicious@gmail.com>
2025-10-10 10:30:16 -0700

    add toast message, clean up readability

--- src/components/AuctionList/AuctionList.js
+++ src/components/AuctionList/AuctionList.js
@@ -2,10 +2,13 @@ import { useEffect, useState } from 'react';
 import './AuctionList.css';
 import AuctionCard from './AuctionCard.js';
 import { getAuctions } from '../../services/fetch-auctions.js';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 export default function AuctionList() {
   const [auctions, setAuctions] = useState([]);
   const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchAuctions = async () => {
@@ -17,6 +20,13 @@ export default function AuctionList() {
         setAuctions(data);
       } catch (err) {
         console.error('Error fetching auctions', err);
+        toast.error(`Error saving auction: ${err.message}`, {
+          theme: 'colored',
+          draggable: true,
+          draggablePercent: 60,
+          toastId: 'auction-list-1',
+          autoClose: false,
+        });
       } finally {
         setLoading(false);
       }
@@ -37,16 +47,16 @@ export default function AuctionList() {
   return (
     <div className="messages-container">
       <div className="messages-content">
+        <button onClick={() => navigate('/admin/auctions')}>Add/ Edit Auctions</button>
         <div className="messages-header">
           <h1>Glass Art Auctions</h1>
           <p>Bid, watch, or buy instantly.</p>
         </div>
-
         <div className="auction-list">
           {auctions.length === 0 ? (
             <p>No active auctions right now.</p>
           ) : (
-            auctions.map((a) => <AuctionCard key={a.id} auction={a} />)
+            auctions.map((auction) => <AuctionCard key={auction.id} auction={auction} />)
           )}
         </div>
       </div>
