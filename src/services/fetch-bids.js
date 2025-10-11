e672694f541da418e3fbf01a267479661bc1bfde
kevinnail <fumalicious@gmail.com>
2025-10-10 23:49:39 -0700

    add placeBid and buyItNow functions

--- src/services/fetch-bids.js
+++ src/services/fetch-bids.js
@@ -23,3 +23,51 @@ export async function getBids(id) {
     throw new Error(e.message || 'Error in getAuctions');
   }
 }
+
+export async function placeBid({ auctionId, userId, bidAmount }) {
+  try {
+    const resp = await fetch(`${BASE_URL}/api/v1/bids`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ auctionId, userId, bidAmount }),
+      credentials: 'include',
+    });
+
+    const data = await resp;
+    if (resp.ok) {
+      return data;
+    } else {
+      throw new Error(data.message);
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+export async function buyItNow({ auctionId, userId }) {
+  try {
+    const resp = await fetch(`${BASE_URL}/api/v1/bids/buy-it-now`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ auctionId, userId }),
+      credentials: 'include',
+    });
+
+    const data = await resp;
+    if (resp.ok) {
+      return data;
+    } else {
+      throw new Error(data.message);
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
